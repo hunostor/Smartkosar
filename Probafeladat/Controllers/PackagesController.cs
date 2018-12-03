41471bc83385b6f265440465c70764dcff35d78d
Poroszkai Attila <poroszkai.attila@gmail.com>
2018-12-03 15:06:00 +0100

    Package controller and views

---
+++ Probafeladat/Controllers/PackagesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Probafeladat.Data;
+using Probafeladat.Models;
+
+namespace Probafeladat.Controllers
+{
+    public class PackagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PackagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Packages
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Packages.Include(p => p.ShippingState);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Packages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var package = await _context.Packages
+                .Include(p => p.ShippingState)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            return View(package);
+        }
+
+        // GET: Packages/Create
+        public IActionResult Create()
+        {
+            ViewData["ShippingStateID"] = new SelectList(_context.ShippingStates, "ID", "Description");
+            return View();
+        }
+
+        // POST: Packages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Identifier,ShippingStateID")] Package package)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(package);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ShippingStateID"] = new SelectList(_context.ShippingStates, "ID", "Description", package.ShippingStateID);
+            return View(package);
+        }
+
+        // GET: Packages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var package = await _context.Packages.FindAsync(id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+            ViewData["ShippingStateID"] = new SelectList(_context.ShippingStates, "ID", "Description", package.ShippingStateID);
+            return View(package);
+        }
+
+        // POST: Packages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Identifier,ShippingStateID")] Package package)
+        {
+            if (id != package.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(package);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PackageExists(package.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ShippingStateID"] = new SelectList(_context.ShippingStates, "ID", "Description", package.ShippingStateID);
+            return View(package);
+        }
+
+        // GET: Packages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var package = await _context.Packages
+                .Include(p => p.ShippingState)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            return View(package);
+        }
+
+        // POST: Packages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var package = await _context.Packages.FindAsync(id);
+            _context.Packages.Remove(package);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PackageExists(int id)
+        {
+            return _context.Packages.Any(e => e.ID == id);
+        }
+    }
+}
